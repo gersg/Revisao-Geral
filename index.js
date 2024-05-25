b0f051882a066c2e2a226a8f4428b9fb26a93934
Gerson Espindola <gersgdev@gmail.com>
2024-05-25 16:39:16 -0300

    atualiza-readme.MD-e-cria-index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+/*
+Variaveis:
+conceitos: variaveis sao usadas para armazenar dados, e para manipula-los.
+
+Exemplo:
+let nome = 'João';
+let idade = 30;
+let altura = 1.75;
+let estudando = true;
+
+console.log(nome, idade, altura, estudando);
+
+Constantes:
+constante: uma vez definida, não pode ser alterada.
+
+Exemplo:
+const nome = 'João';
+const idade = 30;
+const altura = 1.75;
+const estudando = true;
+
+console.log(nome, idade, altura, estudando);
+
+
+Condicionais:
+conceitos: if, else if, else, switch.
+sao usados para controlar o fluxo de execucao de um programa, fazer
+comparações e retornar um valor.
+
+Exemplo:
+if (idade >= 18) {
+    console.log('Pode entrar');
+} else {
+    console.log('Não pode entrar');
+}
+
+Switch:
+conceito: switch e case.
+sao usados para comparar uma variavel com um conjunto de valores e retornar um valor.
+
+Exemplo:
+switch (idade) {
+    case 18:
+        console.log('Pode entrar');
+        break;
+    case 19:
+        console.log('Pode entrar');
+        break;
+    default:
+        console.log('Não pode entrar');
+        break;
+
+        
+}
+
+
+Arrays:
+conceitos: arrays sao estruturas de dados que permitem armazenar
+varios valores em uma unica variavel.
+
+Exemplo:
+let cores = ['vermelho', 'amarelo', 'azul'];
+console.log(cores[0]);
+
+Objetos:
+conceitos: objetos sao estruturas de dados que permitem armazenar
+varios valores em uma unica variavel.
+
+Exemplo:
+let pessoa = {
+    nome: 'João',
+    idade: 30,
+    altura: 1.75,
+    estudando: true
+};
+console.log(pessoa.nome);
+
+*/
